2fe73df289b6489889f5bbb328ed740e35dd17e6
EFENDI <fendikrama56@gmail.com>
2023-11-24 17:28:37 +0700

    first commit

---
+++ entity/item.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"time"
+)
+
+type Item struct {
+	ItemId      int
+	ItemCode    string
+	Quantity    int
+	Description string
+	OrderId     int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	IsDeleted   bool
+	DeletedAt   *time.Time
+}
---
+++ entity/order.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type Order struct {
+	OrderId      int
+	CustomerName string
+	OrderedAt    time.Time
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	IsDeleted    bool
+	DeletedAt    *time.Time
+}
